78abf27ba7e25be3f0e397fbace2f7cc7c78260b
atmka <corp.dvd@gmail.com>
2019-10-20 01:11:26 +0400

    Initial commit

---
+++ src/main/java/ru/barsim/Main.java
@@ -0,0 +1,14 @@
+package ru.barsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Вход-выход</title>
+
+</head>
+<body>
+	Хелоу
+</body>
+</html>
